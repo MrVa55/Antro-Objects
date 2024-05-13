566cdd265c2d06a650558a81d5ac2390a2583074
Thomas Vass <thomasstefanvass@gmail.com>
2024-05-13 14:43:22 +0200

    removed s

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source .venv/bin/activate
-python3 antro-objects.py --warmup-file jfk.wav  --host localhost  --port 4500 --model medium.en --vad
+python3 antro-object.py --warmup-file jfk.wav  --host localhost  --port 4500 --model medium.en --vad
